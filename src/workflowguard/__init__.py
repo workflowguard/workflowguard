7aa56ae27a05a8f6f4fe4df2e18ae7b991d37b97
Brendan HomePC <brenoates@gmail.com>
2015-06-13 18:03:54 +0100

    added Action and FlowUnit

--- src/workflowguard/__init__.py
+++ src/workflowguard/__init__.py
@@ -4,6 +4,57 @@ __version__ = "0.0.1"
 class State:
     """A State class"""
     def __init__(self, name, label=''):
+        """Create an instance of State
+        Arguments:
+        name: -- the name of this State
+        label: -- a short description of the state
+        """
         self.name = name
         self.label = label
 
+
+class FlowUnit:
+    """FlowUnit Class
+    This is a core class of the library. A flow_unit instance holds the current state. It is passed to the a
+    relevant Action instance when the actions perform method is called. The available Action objects are defined by the
+    Transition class and are dependant on the flow_units current state.
+    """
+    def __init__(self, name, *args, **kwargs):
+        """Create an instance of FlowUnit
+        Arguments:
+        name: -- the name of this flow_unit
+        Keyword arguments:
+        state -- initial state
+        """
+        self.name = name
+        self.state = kwargs.get('state', State('initial', 'initial state'))
+
+
+class Action:
+    """
+    Action Class
+    An instance of this class should be created for each action that is required by the system. An action carries out
+    processing on a FlowUnit instance and is what causes the State instance of a FlowUnit instance to change.
+    """
+    def _default_function(flow_unit, *args, **kwargs):
+        """Return an informative message as we shouldn't ever need this function. """
+        print("default function your action has been created with no action function, surely you didn't mean to do this!")
+
+    def __init__(self, name, *args, **kwargs):
+        """Create an instance of Action
+        Arguments:
+        name: -- the name of this action
+        Keyword arguments:
+        action -- the function that will be performed when this actions perform action is called
+        """
+        self.name = name
+        self.action = kwargs.get('action', self._default_function)
+
+    def perform(self, flow_unit, *args, **kwargs):
+        """Carries out the action on the provided flow_unit
+        Arguments:
+        flow_unit - the flow_unit instance that will have the processing caried out upon it.
+        Keyword arguments:
+        as required by the action function for this Action instance.
+        """
+        self.action(flow_unit, *args, **kwargs)
--- tests/test_workflowguard.py
+++ tests/test_workflowguard.py
@@ -4,6 +4,8 @@ from click.testing import CliRunner
 from workflowguard.__main__ import main
 
 from workflowguard import State
+from workflowguard import Action
+from workflowguard import FlowUnit
 
 def test_main():
     runner = CliRunner()
@@ -12,13 +14,35 @@ def test_main():
     assert result.output == '()\n'
     assert result.exit_code == 0
 
-def test_create_a_state():
-    name1 = 'test1'
-    name2 = 'test2'
-    label1 =  'test label 1'
-    state_object1 = State(name1)
-    state_object2 = State(name1, label1)
+def test_state():
+    name1 = 'state1'
+    label1 =  'state label 1'
+    state_object1 = State(name1, label1)
     assert state_object1.name == name1
-    assert state_object1.label == ''
-    assert state_object2.label == label1
+    assert state_object1.label == label1
 
+def test_flow_unit():
+    name1 = 'flow_unit1'
+    name2 = 'flow_unit2'
+    flow_unit1 = FlowUnit(name1)
+    assert flow_unit1.name == name1
+    assert flow_unit1.state.name == 'initial'
+    state_object1 = State('state1', 'state label 1')
+    flow_unit2 = FlowUnit(name2, state=state_object1)
+    assert flow_unit2.state.name == state_object1.name
+
+
+def test_action():
+    name1 = 'action1'
+    name2 = 'action2'
+    action2_text = 'action2 text'
+    action1 = Action(name1)
+    assert action1.name == name1
+    flow_unit1 = FlowUnit('flow_unit1')
+    action1.perform(flow_unit1)
+    def change_state(flow_unit, *args, **kwargs):
+        flow_unit.state = kwargs.get('state' or None)
+    second_state = State('2ndState', 'Second State')
+    action2 = Action('action2', action=change_state)
+    action2.perform(flow_unit1, state=second_state)
+    assert flow_unit1.state == second_state
