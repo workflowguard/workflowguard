dbd967162e7232926d1cc8656097c890f3d2fb66
brendan <brendan.oates@amadeus.com>
2015-06-15 17:07:50 +0100

    tidied up the unit tests

--- src/workflowguard/middleware.py
+++ src/workflowguard/middleware.py
@@ -5,14 +5,20 @@ This module is used for any common functions that can be used to access the Work
 """
 
 def dummy_action(flow_unit):
+    """
+    This a dummy function used by the unit tests it does nothing but returns what is passed to it.
+    :param flow_unit:
+    :return: flow_unit
+    """
     return flow_unit
 
 def change_flow_unit_state(flow_unit, state, *args, **kwargs):
     """
-    :param flow_unit: The FlowUnit whose state is changing
+    :param
+    flow_unit: a FlowUnit object whose state is to be changed
+    state: a State object that flow_unit.state will now be set to.
     :param args:
     :param kwargs:
-      'state': a State object that flow_unit.state will now hold.
     :return: None
     """
     flow_unit.state = state
